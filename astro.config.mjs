0b9a211b68128dfd5a067aebad5805dc123da9cb
edgarsegundo <edgar.segundo@gmail.com>
2025-09-06 21:25:13 -0300

    Checkpoint at 06-Sep-25 21:25:12

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+
+// Constants
+const SITE_URL = 'https://fastvistos.com.br';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: SITE_URL,
+    vite: {
+        plugins: [tailwindcss()]
+    }
+});
