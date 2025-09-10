bbd1e82db62fe80c42df251fd93ce41efbb68361
edgarsegundo <edgar.segundo@gmail.com>
2025-09-10 19:34:37 -0300

    Checkpoint at 10-Sep-25 19:34:37

--- src/config/pages/index.ts
+++
@@ -1,9 +0,0 @@
-// Export all page-specific SEO configurations
-export { homePageSEO } from './home';
-export { blogPageSEO } from './blog';
-export { aboutPageSEO } from './about';
-export { contactPageSEO } from './contact';
-
-// Re-export the config utilities for convenience
-export { createPageSEO, type PageSEOConfig } from './pageConfig';
-export { baseSEOConfig } from '../seoConfig';
