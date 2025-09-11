2257946e3c031c0543d31a899ecc9ebd30982665
edgarsegundo <edgar.segundo@gmail.com>
2025-09-11 17:43:20 -0300

    Checkpoint at 11-Sep-25 17:43:20

---
+++ multi-sites/sites/p2digital/content.config.ts
@@ -0,0 +1,19 @@
+import { defineCollection, z } from 'astro:content';
+
+// Define content collections for p2digital
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishDate: z.date(),
+    author: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    image: z.string().optional(),
+    draft: z.boolean().optional(),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
