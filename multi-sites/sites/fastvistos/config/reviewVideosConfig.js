876447fbafcc56b2f4ee286d800851780ce4f982
edgarsegundo <edgar.segundo@gmail.com>
2025-09-19 02:54:43 -0300

    Checkpoint at 19-Sep-25 02:54:43

---
+++ multi-sites/sites/fastvistos/config/reviewVideosConfig.js
@@ -0,0 +1,51 @@
+// FastVistos Review Videos Configuration
+export const reviewVideos = [
+  {
+    videoId: 'p-NECzx53kI',
+    title: 'Depoimento 1',
+    thumbnail: 'https://img.youtube.com/vi/p-NECzx53kI/hqdefault.jpg',
+    url: 'https://www.youtube.com/watch?v=p-NECzx53kI',
+  },
+  {
+    videoId: 'YxfB-J3K8KU',
+    title: 'Depoimento 2',
+    thumbnail: 'https://img.youtube.com/vi/YxfB-J3K8KU/hqdefault.jpg',
+    url: 'https://www.youtube.com/watch?v=YxfB-J3K8KU',
+  },
+  {
+    videoId: '-m3GVyU8oXI',
+    title: 'Depoimento 3',
+    thumbnail: 'https://img.youtube.com/vi/-m3GVyU8oXI/hqdefault.jpg',
+    url: 'https://www.youtube.com/watch?v=-m3GVyU8oXI',
+  },
+  {
+    videoId: 'VtWl9V-E45k',
+    title: 'Depoimento 4',
+    thumbnail: 'https://img.youtube.com/vi/VtWl9V-E45k/hqdefault.jpg',
+    url: 'https://www.youtube.com/watch?v=VtWl9V-E45k',
+  },
+  {
+    videoId: 'QoU1U_Wkob4',
+    title: 'Depoimento 5',
+    thumbnail: 'https://img.youtube.com/vi/QoU1U_Wkob4/hqdefault.jpg',
+    url: 'https://www.youtube.com/watch?v=QoU1U_Wkob4',
+  },
+  {
+    videoId: 'yiI4CmfSTF0',
+    title: 'Depoimento 6',
+    thumbnail: 'https://img.youtube.com/vi/yiI4CmfSTF0/hqdefault.jpg',
+    url: 'https://www.youtube.com/watch?v=yiI4CmfSTF0',
+  },
+  {
+    videoId: 'G9yUPtTf4s4',
+    title: 'Depoimento 7',
+    thumbnail: 'https://img.youtube.com/vi/G9yUPtTf4s4/hqdefault.jpg',
+    url: 'https://www.youtube.com/watch?v=G9yUPtTf4s4',
+  },
+  {
+    videoId: 'ZqQ0YNJa_pQ',
+    title: 'Depoimento 8',
+    thumbnail: 'https://img.youtube.com/vi/ZqQ0YNJa_pQ/hqdefault.jpg',
+    url: 'https://www.youtube.com/watch?v=ZqQ0YNJa_pQ',
+  },
+];
