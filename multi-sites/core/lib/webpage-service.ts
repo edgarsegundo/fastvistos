345ef68035fb93576270dedc84c255573d3c6605
edgarsegundo <edgar.segundo@gmail.com>
2025-09-24 12:08:35 -0300

    Checkpoint at 24-Sep-25 12:08:34

--- multi-sites/core/lib/webpage-service.ts
+++ multi-sites/core/lib/webpage-service.ts
@@ -22,8 +22,11 @@ export class WebPageService {
             console.error('[DEBUG] prisma is undefined or null!');
             throw new Error('Prisma client is not initialized');
         }
-        // if the businessId has dashes, strip them for the query
+
+        // if those have dashes, strip them for the query
         businessId = businessId.replace(/-/g, '');
+        updatableUuid = updatableUuid.replace(/-/g, '');
+
         return await prisma.$transaction(async (tx: any) => {
             // 1. Find the web_page by relative_path and business_id
             let webPage;
