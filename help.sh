186a7025a847edc868807642f17b92696ab29d22
edgarsegundo <edgar.segundo@gmail.com>
2025-09-26 18:12:29 -0300

    Checkpoint at 26-Sep-25 18:12:28

--- help.sh
+++ help.sh
@@ -1,3 +1,21 @@
+# Usage: ./help.sh dbshell
+# Opens a MySQL shell in the running 'mysql' container using credentials from .env
+
+if [ "$1" = "dbshell" ]; then
+  # Load env vars from .env
+  export $(grep '^DB_USER=' .env | xargs)
+  export $(grep '^DB_PASSWORD=' .env | xargs)
+  export $(grep '^DB_NAME=' .env | xargs)
+
+  # Remove quotes if present
+  DB_USER=$(echo $DB_USER | tr -d '"')
+  DB_PASSWORD=$(echo $DB_PASSWORD | tr -d '"')
+  DB_NAME=$(echo $DB_NAME | tr -d '"')
+
+  # Enter MySQL shell inside the container
+  docker exec -it mysql bash -c "mysql -u$DB_USER -p$DB_PASSWORD $DB_NAME"
+  exit $?
+fi
 #!/bin/bash
 
 while true; do
@@ -5,6 +23,7 @@ while true; do
   echo "1) Publish msitesapp"
   echo "2) Run test-findUnique.js"
   echo "3) Git commit and push"
+  echo "4) Enter MySQL shell (dbshell)"
   echo "q) Quit"
   read -p "Choose an option: " opt
 
@@ -26,6 +45,16 @@ while true; do
       git add .
       git commit -m "Auto-commit"
       git push
+      ;;
+    4)
+      # Enter MySQL shell using env vars from .env
+      export $(grep '^DB_USER=' .env | xargs)
+      export $(grep '^DB_PASSWORD=' .env | xargs)
+      export $(grep '^DB_NAME=' .env | xargs)
+      DB_USER=$(echo $DB_USER | tr -d '"')
+      DB_PASSWORD=$(echo $DB_PASSWORD | tr -d '"')
+      DB_NAME=$(echo $DB_NAME | tr -d '"')
+      docker exec -it mysql bash -c "mysql -u$DB_USER -p$DB_PASSWORD $DB_NAME"
       ;;
     q)
       echo "Goodbye!"
