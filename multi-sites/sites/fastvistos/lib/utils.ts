798394c5258f8d8a1eee1650b95168a5d01ef8a9
edgarsegundo <edgar.segundo@gmail.com>
2025-10-02 11:14:29 -0300

    Checkpoint at 02-Oct-25 11:14:29

--- multi-sites/sites/fastvistos/lib/utils.ts
+++ multi-sites/sites/fastvistos/lib/utils.ts
@@ -2,3 +2,12 @@ export function normalizeAssetsUrlBase(assetsUrlBase: string): string {
     let base = assetsUrlBase.replace(/\/$/, '');
     return base.startsWith('http') ? base : `https://${base}`;
 }
+
+export function slugify(str: string): string {
+    return str
+        .normalize('NFD') // Remove accents
+        .replace(/[\u0300-\u036f]/g, '')
+        .toLowerCase()
+        .replace(/[^a-z0-9]+/g, '-') // Replace non-alphanumeric with hyphen
+        .replace(/^-+|-+$/g, '');    // Trim hyphens
+}
