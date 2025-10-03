053500d678fa67a65d117bb3987670bb53d08e60
edgarsegundo <edgar.segundo@gmail.com>
2025-10-03 19:46:07 -0300

    Checkpoint at 03-Oct-25 19:46:07

--- multi-sites/sites/fastvistos/config/faqConfig.js
+++ multi-sites/sites/fastvistos/config/faqConfig.js
@@ -32,14 +32,6 @@ export const faqData = [
         question: 'Posso aplicar para visto mesmo com negativa anterior?',
         answer: 'Sim! Analisamos cada caso individualmente e desenvolvemos estratégias específicas para superar negativas anteriores.',
     },
-
-    {
-        question: 'question test 1?',
-        answer: 'answer test 1',
-    },
-
-
-
 ];
 
 export function formatAnswerForDisplay(answer) {
