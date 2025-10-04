15269de8a867575a85cb1a226345e378fd692785
Edgar Segundo <edgar.segundo@gmail.com>
2025-10-04 20:28:29 -0300

    Fastvistos conf - missing part (#27)

--- multi-sites/sites/fastvistos/site-config.ts
+++ multi-sites/sites/fastvistos/site-config.ts
@@ -193,7 +193,7 @@ export const siteConfig: SiteConfig = {
     },
 
     verification: {
-        googleSiteVerification: 'wPmMtzby8Xpg',
+        googleSiteVerification: '',
     },
 
     features: {
