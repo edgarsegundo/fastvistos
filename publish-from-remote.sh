ee40168f8a08fadf8ae62c49f7230acae03bb1d6
edgarsegundo <edgar.segundo@gmail.com>
2025-10-21 07:53:18 -0300

    Checkpoint at 21-Oct-25 07:53:18

--- publish-from-remote.sh
+++ publish-from-remote.sh
@@ -3,7 +3,7 @@
 ## Run these commands locally to generate blog content and download images
 node generate-blog-content.js fastvistos
 npm run download-images:fastvistos
-pu
+~/Repos/pu.sh
 
 ## now I need to run the following sripts on the VPS from my local machine
 ssh edgar@72.60.57.150 'cd /home/edgar/Repos/fastvistos && npm run build:fastvistos'
