6ada8073c2d313e00cf341c741992527fb02ef9f
edgarsegundo <edgar.segundo@gmail.com>
2025-10-30 16:47:38 -0300

    Checkpoint at 30-Oct-25 16:47:38

--- multi-sites/core/msitesapp/api/api.js
+++ multi-sites/core/msitesapp/api/api.js
@@ -2,7 +2,7 @@
 import { extractReadableText } from '../../dist/lib/txtify.js';
 import { WebPageService } from '../../dist/lib/webpage-service.js';
 import { reescreverArtigo } from './news-article-generator.js';
-import { openai } from '../openai-client.js'; // adjust path as needed
+// import { openai } from '../openai-client.js'; // adjust path as needed
 
 // reescreverArtigo
 import express from 'express';
@@ -314,7 +314,7 @@ app.post('/publish-article', async (req, res) => {
 
     console.log('publish-article (1)  | artigo1: ', artigo1);
 
-    let newArticle =  reescreverArtigo(openai, artigo1, "xxx");
+    // let newArticle =  reescreverArtigo(openai, artigo1, "xxx");
     console.log('publish-article (2)  | newArticle: ', newArticle);
 
     // const text2 = await extractReadableText(url2);
