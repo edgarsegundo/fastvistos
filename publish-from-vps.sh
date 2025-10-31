03e56c36cadd3c12c84c9c1e5b7eb8b25c602728
edgarsegundo <edgar.segundo@gmail.com>
2025-10-31 18:13:21 -0300

    Checkpoint at 31-Oct-25 18:13:21

--- publish-from-vps.sh
+++ publish-from-vps.sh
@@ -8,16 +8,17 @@ if [ -z "$SITEID" ]; then
 fi
 
 # Run these commands locally to generate blog content and download images
-## Generate blog content and download images
+# Generate blog content and download images
 node generate-blog-content.js "$SITEID"
-## Download images
+
+# Download images
 npm run download-images:"$SITEID"
 
 # Now run the following scripts on the VPS from your local machine
 # Restaura e atualiza o código no VPS
 cd /home/edgar/Repos/fastvistos && git restore . && git pull
 
-## Build site on VPS
+# Build site on VPS
 npm run build:"$SITEID"
 
 # Deploy site on VPS
