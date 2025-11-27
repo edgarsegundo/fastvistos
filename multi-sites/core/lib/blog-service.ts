01046aa2ed7f0881673801fbbabe951e19a1c803
edgarsegundo <edgar.segundo@gmail.com>
2025-11-27 11:41:26 -0300

    Checkpoint at 27-Nov-25 11:41:26

--- multi-sites/core/lib/blog-service.ts
+++ multi-sites/core/lib/blog-service.ts
@@ -95,7 +95,7 @@ export class BlogService {
         try {
             const businessId = this.getBusinessId();
             const now = new Date();
-            return await prisma.blog_article.findUnique({
+            return await prisma.blog_article.findFirst({
                 where: {
                     id: id,
                     business_id: businessId,
